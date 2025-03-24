ff8539f090e9b7e53ff761c13680cb6de217654b
Kassandra <kassandraiatrou@gmail.com>
2025-03-24 23:55:27 +0000

    completed task 0 which prints char, string and %

--- test/_printf.c
+++ test/_printf.c
@@ -1,65 +1,54 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-
+#include <stdlib.h>
+#include "main.h"
 /**
- * main -  Task 0  _printf 
- * Return: 0
- */
- 
- 
- 
-int main(void)
-{
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
-
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-
-	return (0);
-}
-
+ *
+*/
 int _printf(const char *format, ...)
 {
-	int i, j;
+	int i, j, len = 0/*keep track of the num of characters printed*/;
 	va_list args;
 	va_start(args, format);
-
+	
+	/*loop through the format string*/
 	for (i = 0; format[i] != '\0'; i++)
 	{
+		/*if the '%' is encountered*/
 		if (format[i] == '%')
 		{
-			i++;
-		if (format[i] == 'c')
-		{
-			char c = (char) va_arg(args, int);
-			_putchar(c);
+			i++;/*then go to the next position after '%'*/
+			if (format[i] == 's')
+			{
+				/*str arguement*/
+				char* str = va_arg(args, char*);
+				for (j = 0; str[j] != '\0'; j++)/*loop through the string*/
+				{
+					_putchar(str[j]);
+					len++;/*update len with the number of chars printed*/
+				}
+			}
+			if (format[i] == 'c')
+			{
+				/*char arguement*/
+				char c = (char) va_arg(args, int);
+				_putchar(c);
+			}
+			if (format[i] == '%')
+			{
+				_putchar('%');/*simply print '%'*/
+			}
 		}
-		else if (format[i] == 's')
+		else
 		{
-			char* str = va_arg(args, char*);
-			for (j = 0; str[j] != '\0'; j++)
-			_putchar(str[j]);
-		}
-		else 
-		{
-			(format[i] == '%');
-			char c = (char) va_arg(args, int);
-			_putchar(c);
-		}
+			/*just print the regular string*/
+			_putchar(format[i]);
+			len++; /*keep track of the number of chars printed*/
 		}
 	}
-	return(0);
+	va_end(args);/*clean up*/
+	return (len);/*return total len of every char printed*/
 }
 
 int _putchar(char c)
