d8de2fd81af9e4af825a273c1ba5d36262892248
Kassandra <kassandraiatrou@gmail.com>
2025-03-26 23:25:11 +0000

    cleaning up code for Betty

--- _printf.c
+++ _printf.c
@@ -4,15 +4,17 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *
+ * _printf - prints anything
+ * @format: type of specifier being used
+ * Return: count
 */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	
+
 	if (format == NULL)
-	{	
+	{
 		return (-1);
 	}
 	va_start(args, format);
@@ -31,15 +33,16 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 's')
 			{
-				count+= print_str(args);
+				count += print_str(args);
 			}
 			else if (*format == '%')
 			{
-				count+= _putchar('%');
+				count += _putchar('%');
 			}
 			else if (*format == 'i' || *format == 'd')
 			{
 				int num = va_arg(args, int);
+
 				print_number(num, &count);
 			}
 			else /*invalid specifier */
